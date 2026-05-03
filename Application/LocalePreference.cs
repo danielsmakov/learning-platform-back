10b864c005584f8392ff6f5dde3bd3ad2e560268
Aldiyar <aldiyar.zhartybek@narxoz.kz>
2026-05-03 21:54:39 +0500

    document Accept-Language in Swagger; programs header binding; client HTTP examples

--- Application/LocalePreference.cs
+++ Application/LocalePreference.cs
@@ -1,6 +1,6 @@
 namespace LearningPlatform.Application;
 
-/// <summary>Парсинг заголовка Accept-Language (G2/B6); основная локаль en или ru.</summary>
+/// <summary>Парсинг заголовка Accept-Language (G2/B6); основная локаль en или ru. Заголовок описан в OpenAPI (Swagger) на соответствующих GET.</summary>
 public static class LocalePreference
 {
     /// <summary>Возвращает <c>ru</c>, если первый языковой тег начинается с ru; иначе <c>en</c>.</summary>
